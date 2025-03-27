227fc85e9865393fa476ce9e14634c9c668ccdc9
FrankH <4848285@qq.com>
2025-03-28 00:25:54 +0800

    Add TestFile reader.

---
+++ test/WeixinOpen.UnitTest/TestFramework/TestFile.cs
@@ -0,0 +1,15 @@
+﻿namespace Myvas.AspNetCore.Authentication.WeixinOpen.Tests;
+
+public static class TestFile
+{
+    public static string GetTestFilePath(string fileName)
+    {
+        var currentDir = Directory.GetCurrentDirectory();
+        return Path.Combine(currentDir, "Data", fileName);
+    }
+    public static string ReadAllText(string fileName)
+    {
+        var path = GetTestFilePath(fileName);
+        return File.ReadAllText(path);
+    }
+}
